2231f0a5994fb87292bb8aae1641969e59eb40cf
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-01-23 01:21:19 +0700

    admin

---
+++ src/components/header_admin.js
@@ -0,0 +1,12 @@
+const HeaderAdminx = {
+    render() {
+        return /* html */`
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-center text-3xl font-bold text-gray-900">Admin</h1> 
+            </div>
+        </header>`;
+    },
+};
+
+export default HeaderAdminx;
