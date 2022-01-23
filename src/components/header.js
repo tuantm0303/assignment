fcf3d94447b0768638e0407a700d9c67529311f7
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-01-23 15:12:43 +0700

    header

--- src/components/header.js
+++ src/components/header.js
@@ -10,25 +10,25 @@ const Header = {
                 </div>
                 <div class="nav">
                     <ul class="flex p-5 text-center">
-                        <li class="pr-10 hover:opacity-70 font-semibold"><a href="/outlet">Outlet</a></li>
-                        <li class="pr-10 hover:opacity-70 font-semibold"><a href="/wardrobe">Tủ đồ</a></li>
-                        <li class="pr-10 hover:opacity-70 font-semibold"><a href="/product">Sản Phẩm</a></li>
-                        <li class="pr-10 hover:opacity-70 font-semibold"><a href="/introduce">Về Coolmate</a></li>
-                        <li class="pr-10 hover:opacity-70 font-semibold"><a href="/size">Chọn Size</a></li>
+                        <li class="pr-10 hover:opacity-70 hover:text-red-500 font-semibold"><a href="/outlet">Outlet</a></li>
+                        <li class="pr-10 hover:opacity-70 hover:text-red-500 font-semibold"><a href="/wardrobe">Tủ đồ</a></li>
+                        <li class="pr-10 hover:opacity-70 hover:text-red-500 font-semibold"><a href="/product">Sản Phẩm</a></li>
+                        <li class="pr-10 hover:opacity-70 hover:text-red-500 font-semibold"><a href="/introduce">Về Coolmate</a></li>
+                        <li class="pr-10 hover:opacity-70 hover:text-red-500 font-semibold"><a href="/size">Chọn Size</a></li>
                     </ul>
                 </div>
                 <div class="header-action flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-5" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-5 hover:opacity-70 hover:text-red-500" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-5" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-5 hover:opacity-70 hover:text-red-500" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:opacity-70 hover:text-red-500" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
