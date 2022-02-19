603f48a76f38dd166e532e8ae36ec7b2a06e8067
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-02-19 20:23:49 +0700

    category

---
+++ src/pages/admin/category/edit.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-await-in-loop */
+import { get, update } from "../../../api/category";
+import HeaderAdmin from "../../../components/header_admin";
+import NavAdmin from "../../../components/nav_admin";
+
+const EditCategory = {
+    async render(id) {
+        const { data } = await get(id);
+        return /* html */ `
+        ${HeaderAdmin.render()}
+        ${NavAdmin.render()}
+        <div class="min-h-full">
+            <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="lg:flex lg:items-center lg:justify-between">
+                    <div class="flex-1 min-w-0">
+                        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Sửa danh mục</h2>
+                    </div>
+                    <div class="mt-5 flex lg:mt-0 lg:ml-4">
+                        <a href="/#/admin/category/index" class="sm:ml-3">
+                        <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="-ml-1 mr-2 h-5 w-5"><circle cx="12" cy="12" r="10"></circle><polyline points="12 8 8 12 12 16"></polyline><line x1="16" y1="12" x2="8" y2="12"></line></svg>
+                            Quay lại
+                        </button>
+                        </a>
+                    </div>
+                    
+                </div>
+        
+            </div>
+            </header>
+            <main>
+                <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                    <!-- Replace with your content -->
+                    <div class="px-4 py-6 sm:px-0">
+                        <div class="border-4 border-dashed border-gray-200 rounded-lg">                    
+                            <div class="mt-5 md:mt-0 md:col-span-2">
+                                <form action="#" id="form-add">
+                                    <div class="shadow sm:rounded-md sm:overflow-hidden">
+                                        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                        <!-- Name -->
+                                            <div>
+                                                <div class="col-span-3 sm:col-span-2">
+                                                    <label for="company-website" class="block text-sm font-medium text-gray-700">Category Name</label>
+                                                    <div class="mt-1 flex rounded-md shadow-sm">
+                                                        <input class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-3" 
+                                                        type="text" name="title" placeholder="Category Name" id="category-name" value="${data.name}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                            <button type="submit"
+                                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                Edit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /End replace -->
+                </div>
+            </main>
+        </div>`;
+    },
+
+    afterRender(id) {
+        const formAdd = document.querySelector("#form-add");
+
+        formAdd.addEventListener("submit", async (e) => {
+            e.preventDefault();
+
+            const postFake = {
+                id,
+                name: document.querySelector("#category-name").value,
+            };
+            update(postFake)
+                .then(() => {
+                    window.location.href = "/#/admin/category/index";
+                });
+        });
+    },
+};
+
+export default EditCategory;
--- src/pages/admin/category/index.js
+++ src/pages/admin/category/index.js
@@ -11,15 +11,25 @@ const Category = {
                 ${HeaderAdmin.render()}
                 ${NavAdmin.render()}
                 <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <!-- This example requires Tailwind CSS v2.0+ -->
-                    <div class="lg:flex lg:items-center lg:justify-between">
-                    <div class="flex-1 min-w-0">
-                        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-                        Quản lý danh mục
-                        </h2>
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="lg:flex lg:items-center lg:justify-between">
+                        <div class="flex-1 min-w-0">
+                            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                            Quản lý danh mục
+                            </h2>
+                        </div>
+                        <div class="mt-5 flex lg:mt-0 lg:ml-4">
+                            <a href="/#/admin/category/add" class="sm:ml-3">
+                            <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="-ml-1 mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                                </svg>
+                                Thêm mới
+                            </button>
+                            </a>
+                        </div>
                     </div>
-                </div>
                 </header>
         
                 <main>
@@ -34,19 +44,10 @@ const Category = {
                                                 ID
                                             </th>
                                             <th scope="col" class="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Product
+                                                Category
                                             </th>
                                             <th scope="col" class="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Price New
-                                            </th>
-                                            <th scope="col" class="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Price Old
-                                            </th>
-                                            <th scope="col" class="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Quaytity
-                                            </th>
-                                            <th scope="col" class="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Status
+                                                Action
                                             </th>
                                         </tr>
                                     </thead>
@@ -65,22 +66,8 @@ const Category = {
                                                 ${item.name}
                                                 </span>
                                             </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-center">
-                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                ${item.priceNew}
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-center">
-                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                ${item.priceOld}
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-center">
-                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                ${item.quantity}
-                                                </span>
-                                            </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                                <a href="/#/admin/category/edit/${item.id}" class="text-white rounded-xl bg-blue-600 py-2 px-4 mr-2 hover:text-black hover:bg-red-600">Edit</a>
                                                 <button data-id="${item.id}" class="btn btn-delete font-semibold border border-blue-600 rounded-xl bg-blue-600 text-white p-2 hover:text-black hover:bg-red-600 hover:border-red-600">Delete</button>
                                             </td>
                                         </tr>`).join("")}
@@ -94,21 +81,21 @@ const Category = {
             </div>`;
     },
 
-    // afterRender() {
-    //     const btns = document.querySelectorAll(".btn");
-    //     btns.forEach((btn) => {
-    //         btn.addEventListener("click", () => {
-    //             const { id } = btn.dataset; // dataset lấy từ data-id="${item.id}"
-    //             // eslint-disable-next-line no-alert
-    //             const confirm = window.confirm("Bạn có muốn xóa không???");
+    afterRender() {
+        const btns = document.querySelectorAll(".btn");
+        btns.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                const { id } = btn.dataset; // dataset lấy từ data-id="${item.id}"
+                // eslint-disable-next-line no-alert
+                const confirm = window.confirm("Bạn có muốn xóa không???");
 
-    //             if (confirm) {
-    //                 remove(id)
-    //                     .then(() => window.location.href = "/#/admin/users/index");
-    //             }
-    //         });
-    //     });
-    // },
+                if (confirm) {
+                    remove(id)
+                        .then(() => window.location.href = "/#/admin/category/index");
+                }
+            });
+        });
+    },
 };
 
 export default Category;
