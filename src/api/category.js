9586161f05d5debd47affdcd54b500709c119220
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-02-19 15:28:39 +0700

    category

---
+++ src/api/category.js
@@ -0,0 +1,30 @@
+import instance from "./instance";
+
+const getAll = () => {
+    const url = "/categorys";
+    return instance.get(url);
+};
+
+const get = (id) => {
+    const url = `/categorys/${id}`;
+    return instance.get(url);
+};
+
+const add = (category) => {
+    const url = `/categorys/`;
+    return instance.category(url, category);
+};
+
+const remove = (id) => {
+    const url = `/categorys/${id}`;
+    return instance.delete(url);
+};
+
+const update = (category) => {
+    const url = `/categorys/${category.id}`;
+    return instance.put(url, category);
+};
+
+export {
+    getAll, get, add, remove, update,
+};
